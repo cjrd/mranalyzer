7f4dc779034852a9a17bfc474830fbed0007953c
Colorado Reed <colorado.j.reed@gmail.com>
2022-11-29 08:46:25 -0800

    Update doxygen index.html

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['make_5fdir_5fif_5fnot_5fexist_0',['make_dir_if_not_exist',['../namespaceutil.html#af2fc2f874bbf826ab3ac847bb6431f07',1,'util']]],
-  ['mars_20rover_20analysis_20tools_1',['Mars Rover Analysis Tools',['../index.html',1,'']]]
+  ['mars_20rover_20analyzer_20_28machine_20learning_20and_20instrument_20autonomy_20group_20_2d_20jpl_29_1',['Mars Rover Analyzer (Machine Learning and Instrument Autonomy Group - JPL)',['../index.html',1,'']]]
 ];
--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['mars_20rover_20analysis_20tools_0',['Mars Rover Analysis Tools',['../index.html',1,'']]]
+  ['mars_20rover_20analyzer_20_28machine_20learning_20and_20instrument_20autonomy_20group_20_2d_20jpl_29_0',['Mars Rover Analyzer (Machine Learning and Instrument Autonomy Group - JPL)',['../index.html',1,'']]]
 ];
